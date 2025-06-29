d7425a9d8a0180f129b7ec57b5909365f0b5ba53
oqla-alrefai <realrefai.rr@gmail.com>
2025-06-29 18:19:21 +0300

    2nd version

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,8 +6,8 @@ export default function Gallery() {
       <h2>Gallery</h2>
       <div className={styles.images}>
         {/* Example images */}
-        <img src="/gallery1.jpg" alt="Event 1" />
-        <img src="/gallery2.jpg" alt="Event 2" />
+        <img src="https://images.unsplash.com/photo-1580582932707-520aed937b7b?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Event 1" />
+        <img src="https://images.unsplash.com/photo-1604134967494-8a9ed3adea0d?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNjaG9vbHxlbnwwfHwwfHx8MA%3D%3D" alt="Event 2" />
       </div>
     </div>
   );
